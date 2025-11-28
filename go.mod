c9210a74499c01fa7dabad863a5808de2e44ea3c
Emilien Marquegnies <marquegniesemilien@gmail.com>
2025-11-28 11:42:50 +0100

    Add : first working version

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.4
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/go-resty/resty/v2 v2.17.0 // indirect
 	github.com/gofiber/fiber/v2 v2.52.10 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
@@ -14,5 +15,6 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"gonion/controllers"
+
+	"github.com/go-resty/resty/v2"
 	"github.com/gofiber/fiber/v2"
 )
 
-func main()  {
+func NpmProxy() {
+	npm_registry := "https://registry.npmjs.org/"
+	local_registry := "http://localhost:3473/"
+
+	client := resty.New()
+	client.BaseURL = npm_registry
+
+	npm_controller := controllers.NewNpmController(client, "./cache/npm", npm_registry, local_registry)
+
 	app := fiber.New()
+
+	app.Get("*", npm_controller.HandleRequest)
+
+	app.Listen(":3473")
+}
+
+func ComposerProxy() {
+	app := fiber.New()
+
+	app.Get("/", func(c* fiber.Ctx) error {
+		return c.SendString("Hello from composer proxy")
+	})
+
+
+	app.Listen(":3500")
+}
+
+func main() {
+	go NpmProxy()
+	go ComposerProxy()
+
+	select {}
 }
